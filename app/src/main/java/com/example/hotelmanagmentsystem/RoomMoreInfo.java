c4b326a509fb77f89401573f95a5e4aa65ac2a33
Mohammed Darawad <moe.darawad@gmail.com>
2021-12-28 23:40:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hotelmanagmentsystem/RoomMoreInfo.java
@@ -0,0 +1,14 @@
+package com.example.hotelmanagmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomMoreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_more_info);
+    }
+}
