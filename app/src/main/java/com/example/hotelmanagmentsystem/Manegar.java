c98960a8aa1a4cdfc26b7c38515ca5cf4e2e2c54
MohamadAlwan <91935195+MohamadAlwan@users.noreply.github.com>
2022-01-10 19:54:36 +0200

    mohamad_Work6

--- app/src/main/java/com/example/hotelmanagmentsystem/Manegar.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/Manegar.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+
+import com.example.hotelmanagmentsystem.pacageViewRoom.ViewRoomForManager;
 
 public class Manegar extends AppCompatActivity {
 
--- app/src/main/java/com/example/hotelmanagmentsystem/pacageViewRoom/ViewRoomForManager.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/pacageViewRoom/ViewRoomForManager.java
@@ -1,4 +1,4 @@
-package com.example.hotelmanagmentsystem;
+package com.example.hotelmanagmentsystem.pacageViewRoom;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -14,9 +15,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.hotelmanagmentsystem.model.ImageURLData;
+import com.example.hotelmanagmentsystem.R;
+import com.example.hotelmanagmentsystem.ReserveList;
 import com.example.hotelmanagmentsystem.model.RequestQueueSingleton;
-import com.example.hotelmanagmentsystem.pacageViewRoom.ViewRoomAdapter;
+import com.example.hotelmanagmentsystem.model.ReservedRoom;
+import com.example.hotelmanagmentsystem.model.Room;
+import com.example.hotelmanagmentsystem.reservedRoomsAdapter;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -27,11 +31,11 @@ import java.util.ArrayList;
 
 public class ViewRoomForManager extends AppCompatActivity {
     private RecyclerView recycler;
+    private InitialRoomInfo[] RoomsList;
     private RequestQueue queue;
-    private RequestQueue queue2;
+    private  String apiURL = "http://10.0.2.2:80/room-all-info.php";
     private Gson gson;
     private int i=1;
-    private String imageApiURL = "http://10.0.2.2/get-images.php?rId=";
     private String URL;
 
     @Override
@@ -40,10 +44,51 @@ public class ViewRoomForManager extends AppCompatActivity {
         setContentView(R.layout.activity_view_room_for_manager);
         recycler=findViewById(R.id.view_room_recycler);
         queue = Volley.newRequestQueue(this);
-        queue2 = Volley.newRequestQueue(this);
-        getData();
-    }
+        gson = new Gson();
+        try {
 
+
+            RequestQueue queue = Volley.newRequestQueue(ViewRoomForManager.this);
+
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, apiURL,
+                    null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    RoomsList = new InitialRoomInfo[response.length()];
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            RoomsList[i] = gson.fromJson(response.getJSONObject(i).toString(), InitialRoomInfo.class);
+                        } catch (JSONException exception) {
+                            Log.d("Error", exception.toString());
+                        }
+
+                    }
+                    recycler.setLayoutManager(new LinearLayoutManager(ViewRoomForManager.this));
+                    ViewRoomAdapter adapter = new ViewRoomAdapter(RoomsList, getApplicationContext());
+                    recycler.setAdapter(adapter);
+                 //   if (RoomsList.length != 0) {
+
+                  /*  } else {
+                        Toast.makeText(ViewRoomForManager.this, "Errrrrrrrrr",
+                                Toast.LENGTH_SHORT).show();
+                    } */
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error", error.toString());
+                }
+            });
+
+            RequestQueueSingleton.getInstance(this).addToRequestQueue(request);
+        } catch (Exception ex) {
+            Log.d("Error", ex.toString());
+            ex.printStackTrace();
+        }
+
+        //getData();
+    }
+/*
     ArrayList<String> caption1 = new ArrayList<>();
     private void getData() {
         String url = "http://10.0.2.2:80/room-all-info.php";
@@ -57,7 +102,7 @@ public class ViewRoomForManager extends AppCompatActivity {
                         JSONObject obj = response.getJSONObject(i);
                         caption1.add( "Room ID: " + obj.getString("rId") + "\n"
                                 +"Capacity: " + obj.getString("capacity")
-                              );
+                        );
                     }catch(JSONException exception){
                         Log.d("Error", exception.toString());
                     }
@@ -77,5 +122,7 @@ public class ViewRoomForManager extends AppCompatActivity {
 
         queue.add(request);
     }
+    */
+
 
 }
