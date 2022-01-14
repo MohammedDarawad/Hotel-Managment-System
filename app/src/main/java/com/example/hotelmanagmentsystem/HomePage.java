4e2307bc6ad9a491a24be1fb6cde3eac9ef4d8cd
Hammam Tawafshah <45073052+HammamT@users.noreply.github.com>
2022-01-14 19:07:01 +0200

    ver1.3

--- app/src/main/java/com/example/hotelmanagmentsystem/HomePage.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/HomePage.java
@@ -2,39 +2,75 @@ package com.example.hotelmanagmentsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.hotelmanagmentsystem.AlwanWork.Sign_Up;
 
 public class HomePage extends AppCompatActivity {
+    private DrawerLayout drawerLayout ;
+    private String UserName ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 
-    public void servicesList(View view) {
+    public void clickmenu(View view) {
+        openDrwawer(drawerLayout);
+    }
+
+    private void openDrwawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void clickLogo(View view) {
+        closeDrwawer(drawerLayout);
+    }
+
+    private void closeDrwawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void clickSearch(View view) {
         Intent intent = new Intent(this, ServicesList.class);
         startActivity(intent);
     }
 
-    public void login(View view) {
+
+    public void clickservicesList(View view) {
+        Intent intent = new Intent(this, ServicesList.class);
+        startActivity(intent);
+    }
+
+    public void clickLogin(View view) {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
 
-    public void btnClickSignUp(View view) {
+    public void clickSignUp(View view) {
         Intent intent = new Intent(this, Sign_Up.class);
         startActivity(intent);
     }
 
 
-    public void showReservedRoomsList(View view) {
+    public void clickReservedList(View view) {
         Intent intent = new Intent(this, ReserveList.class);
         intent.putExtra("uId", "4");
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrwawer(drawerLayout);
     }
 }
--- app/src/main/java/com/example/hotelmanagmentsystem/SearchResult.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/SearchResult.java
@@ -1,14 +1,136 @@
 package com.example.hotelmanagmentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class SearchResult extends AppCompatActivity {
+
+    Intent intentCom = getIntent();
+    String date1 = intentCom.getStringExtra("date1");
+    String date2 = intentCom.getStringExtra("date2");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        getData();
     }
+
+    public void getData() {
+
+
+        String url = "http://10.0.2.2:80/test/getAllRoom.php?firstdate=" + date1 + "&Seconddate=" + date2;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else {
+
+
+            Thread thread = new Thread(new MyTask(url));
+            thread.start();
+
+        }
+
+    }
+
+
+    private String DownloadText(String URL)
+    {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer))>0) {
+                //---convert the chars to a String---
+                String readString =
+                        String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+        return str;
+    }
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+
+    private class MyTask implements Runnable {
+            private String url;
+
+            public MyTask(String url) {
+                this.url = url;
+            }
+
+            @Override
+            public void run() {
+                final String result = DownloadText(url);
+
+
+
+            }
+        }
+
+
 }
