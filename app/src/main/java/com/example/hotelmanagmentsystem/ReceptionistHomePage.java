ae43b1d71ec7ebc24cb67d8639d03f0957c0dfe0
Mohammed Darawad <moe.darawad@gmail.com>
2022-01-17 19:27:45 +0200

    Initial commit

--- app/src/main/java/com/example/hotelmanagmentsystem/ReceptionistHomePage.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/ReceptionistHomePage.java
@@ -2,12 +2,24 @@ package com.example.hotelmanagmentsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.hotelmanagmentsystem.model.RequestQueueSingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ReceptionistHomePage extends AppCompatActivity {
     private Intent intent;
+    private TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,9 @@ public class ReceptionistHomePage extends AppCompatActivity {
         setContentView(R.layout.activity_receptionest_home_page);
 
         intent = getIntent();
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        getValues();
     }
 
     public void manageRequests(View view) {
@@ -32,5 +47,29 @@ public class ReceptionistHomePage extends AppCompatActivity {
         Intent intent2 = new Intent(this, ChangePassword.class);
         intent2.putExtra("emailAddress", intent.getStringExtra("emailAddress"));
         startActivity(intent2);
+    }
+
+    private void getValues() {
+        String emailAddress = intent.getStringExtra("emailAddress");
+        String apiURL = "http://10.0.2.2/get-receptionest-info-for-homepage.php?emailAddress=" + emailAddress;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, apiURL,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject obj = new JSONObject(response.toString());
+                    tvWelcome.setText("Welcome " + obj.getString("firstName") + "  Id: " + obj.getString("uId"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", error.toString());
+            }
+        });
+
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(request);
     }
 }
