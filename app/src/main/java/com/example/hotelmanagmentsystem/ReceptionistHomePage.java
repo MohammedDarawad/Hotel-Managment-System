3086ecde422025e72c63e21518f876d5daba1228
Mohammed Darawad <moe.darawad@gmail.com>
2022-02-09 00:45:33 +0200

    Initial commit

--- app/src/main/java/com/example/hotelmanagmentsystem/ReceptionistHomePage.java
+++ app/src/main/java/com/example/hotelmanagmentsystem/ReceptionistHomePage.java
@@ -2,14 +2,37 @@ package com.example.hotelmanagmentsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.hotelmanagmentsystem.model.RequestQueueSingleton;
+import com.example.hotelmanagmentsystem.model.RequestedService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.List;
 
 public class ReceptionistHomePage extends AppCompatActivity {
+    private final Gson gson = new Gson();
+    private final String apiURL = "http://10.0.2.2/get-requestedservices.php";
     private Intent intent;
     private TextView tvWelcome;
+    private RecyclerView rvRequests;
+    private TextView tvError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +43,63 @@ public class ReceptionistHomePage extends AppCompatActivity {
         tvWelcome = findViewById(R.id.tvWelcome);
         Intent intent = getIntent();
 
-        tvWelcome.setText("Welcome " + intent.getStringExtra("firstName") + "  Id: " + intent.getStringExtra("uId"));
+        tvWelcome.setText("Welcome " + intent.getStringExtra("firstName"));
+
+        rvRequests = findViewById(R.id.rvRequests);
+        tvError = findViewById(R.id.tvError2);
+
+        getData();
+    }
+
+    private void getData() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, apiURL,
+                null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                List<RequestedService> requestedServicesList = new Gson().fromJson(response.toString(), new TypeToken<List<RequestedService>>() {
+                }.getType());
+
+                if (requestedServicesList.size() != 0) {
+                    rvRequests.setLayoutManager(new LinearLayoutManager(ReceptionistHomePage.this));
+                    RequestedServicesAdapter adapter = new RequestedServicesAdapter(requestedServicesList, getApplicationContext());
+                    rvRequests.setAdapter(adapter);
+                } else {
+                    rvRequests.setVisibility(View.GONE);
+                    tvError.setVisibility(View.VISIBLE);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", error.toString());
+            }
+        });
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(request);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.receptionist_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.changePassword:
+                changePassword();
+                return true;
+            case R.id.logout:
+                logout();
+                return true;
+            case R.id.refreash:
+                getData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void manageRequests(View view) {
@@ -28,14 +107,14 @@ public class ReceptionistHomePage extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void logout(View view) {
+    public void logout() {
         finish();
-        Intent intent = new Intent(this, HomePage.class);
+        Intent intent = new Intent(ReceptionistHomePage.this, HomePage.class);
         startActivity(intent);
     }
 
-    public void changePassword(View view) {
-        Intent intent2 = new Intent(this, ChangePassword.class);
+    public void changePassword() {
+        Intent intent2 = new Intent(ReceptionistHomePage.this, ChangePassword.class);
         intent2.putExtra("uId", intent.getStringExtra("uId"));
         startActivity(intent2);
     }
